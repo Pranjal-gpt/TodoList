4fd7ebdce560d2d2faace98f174a7b9ed7f5a5bf
pranjal gupta <pranjalgupta21088@gmail.com>
2024-06-13 14:50:33 +0530

    modify

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#taskinp').Value.length == 0){
+    if(document.querySelector('#newtask input').value.length == 0){
         alert("please Enter a Task")
     }
     else{
         document.querySelector('#tasks').innerHTML += `
         <div class="task">
         <span id="taskname">
-        ${document.querySelector ('#taskinp'). value}
+        ${document.querySelector ('#newtask input'). value}
         </span>
         <button class="delete">
         <i class="far fa-trash-alt"></i>
@@ -14,5 +14,21 @@ document.querySelector('#push').onclick = function(){
         </div>
 
         `; 
+
+        //deleting a task
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i =0; i< current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value ="";
     }
 }
